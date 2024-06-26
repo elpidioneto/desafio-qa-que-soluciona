3750f8a7f1c8a87d7dc6cad07554e6f907a35bb0
Elpidio Alves Carneiro Neto <38475606+elpidioneto@users.noreply.github.com>
2024-06-26 09:29:23 -0300

    atualização escadaria reduzindo código

--- desafio-01/escadaria.py
+++ desafio-01/escadaria.py
@@ -1,7 +1,8 @@
 try:
     degraus = int(input("digite o número de degraus desejado: "))
+    
     for i in range(1,degraus + 1):
-        espaco = degraus - i
-        print(" " * espaco, "#" * i)
+        print(" " * (degraus - i), "#" * i)
+
 except:
     print("valor informado deve ser númerico refaça a operação")
