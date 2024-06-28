87f17aee12fc4414acecaa765defd27d5dfe5306
Elpidio Alves Carneiro Neto <38475606+elpidioneto@users.noreply.github.com>
2024-06-28 19:15:14 +0000

    adequacao as regras (criar funcao)

--- desafio-01/solucao-desafio01-elpidio.py
+++ desafio-01/solucao-desafio01-elpidio.py
@@ -1,8 +1,11 @@
-try:
-    degraus = int(input("digite o número de degraus desejado: "))
-    
-    for i in range(1,degraus + 1):
-        print(" " * (degraus - i), "#" * i)
+def escadaria(n):
 
-except:
-    print("valor informado deve ser númerico refaça a operação")
+    try:
+        degraus = int(n)
+        for i in range(1,degraus + 1):
+            print(" " * (degraus - i), "#" * i)
+    except:
+        print("valor informado deve ser númerico refaça a operação")
+        escadaria(input("Digite o número de degraus "))
+
+escadaria(input("Digite o número de degraus "))
